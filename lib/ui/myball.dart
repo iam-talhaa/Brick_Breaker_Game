6fc0832796587a542e6dbe50566053afa3aa796c
iam-talhaa <tahakhan4141@gmail.com>
2024-08-08 23:47:08 +0500

    My ball Screen

---
+++ lib/ui/myball.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Myball extends StatefulWidget {
+  final Ballx;
+  final Bally;
+  const Myball({super.key, this.Ballx, this.Bally});
+
+  @override
+  State<Myball> createState() => _MyballState();
+}
+
+class _MyballState extends State<Myball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        height: 15,
+        width: 15,
+        decoration:
+            BoxDecoration(color: Colors.deepPurple, shape: BoxShape.circle),
+      ),
+    );
+  }
+}
